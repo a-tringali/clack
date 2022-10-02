07a3f504fd8b2d8e3c521825941f8030ecf63c55
Andrew Tringali <114039784+a-tringali@users.noreply.github.com>
2022-10-02 15:41:18 -0400

    ClackData

--- ClackData.java
+++ ClackData.java
@@ -1,5 +1,28 @@
 // Data
 
 public class ClackData {
-    
+    private String username;
+    private int type;
+    private Date date;  // date object, probably needs to be made
+
+    // create a user
+    ClackData(username, type){
+
+    }
+
+    // create an anonymous user
+    ClackData(type){
+
+    }
+
+    // default constructor
+    ClackData(){
+
+    }
+
+    // return the type
+    getType(){
+
+    }
+
 }
