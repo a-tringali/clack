ec07698ff42a81d9bd992bffec34be15dd0f4a38
Andrew Tringali <114039784+a-tringali@users.noreply.github.com>
2022-10-03 23:50:59 -0400

    Delete TestDriver.java

--- data/TestDriver.java
+++
@@ -1,6 +0,0 @@
-public class TestDriver {
-    public static void main(String[] args) {
-        MessageClackData test1 = new MessageClackData();
-        FileClackData test2 = new FileClackData();
-    }
-}
