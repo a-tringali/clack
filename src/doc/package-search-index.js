62938b48abebec11c072ff14104efeb04fc062f6
Andrew <114039784+a-tringali@users.noreply.github.com>
2022-11-15 20:59:49 -0500

    .jar builds & Javadocs (Again)

--- src/doc/package-search-index.js
+++ src/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"main"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"data"},{"l":"main"}]
