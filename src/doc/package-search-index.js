2187e8400f2eae094065165a009b6097865ee248
Andrew <114039784+a-tringali@users.noreply.github.com>
2022-12-03 17:07:47 -0500

    Update javadocs

--- src/doc/package-search-index.js
+++ src/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"data"},{"l":"main"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"main"}];updateSearchResults();
